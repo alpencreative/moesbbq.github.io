0a1a95235dcc24e86219967db6c902b339c3c4fd
alpencreative <hazenw@gmail.com>
2017-03-11 11:12:34 -0800

    3/11/17

---
+++ thank-you.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+<head>
+    <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
+    <link rel="STYLESHEET" type="text/css" href="contact.css">
+    <title>Moe's Original BBQ:: Tahoe City</title>
+	<meta charset="UTF-8">
+	<link rel="icon" href="images/moeicon.ico">
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    <header id="blkbkgd">
+    <label class="mobile_toggle" for="toggle">
+      <div class="whiteline line"></div>
+      <div class="whiteline line"></div>
+      <div class="whiteline line"></div>
+    </label>
+    <input type="checkbox" id="toggle">
+      
+      <div class="container clearfix">
+          <nav class="clearfix">
+              <a id="nomobile" class="navlinks" href="menu.html">Menu</a>
+              <div class="dropdown navlinks">
+                 <button id="nomobile" class="dropbtn">Catering</button>
+                 <div class="dropdown-content">
+                  <a class="dcont" href="caterinquire.html">Cater Menu</a>
+                  <a class="dcont" href="contactform.php">Inquiries</a>
+                 </div> 
+              </div>
+        <div id="landlogo">
+          <a id="nomobile" class="navlinks" href="index.html"><img src="images/moes-bbq-large-logo.png"></a>
+        </div>
+        <div class="dropdown navlinks">
+                 <button id="nomobile" class="dropbtn">Music</button>
+                 <div class="dropdown-content">
+                  <a class="dcont" href="events.html">Upcoming Events</a>
+<!--            <a class="dcont" href="lodging.html">Lodging</a> -->
+                 </div> 
+              </div>
+              <a id="nomobile" class="navlinks" href="contact.html">Contact US</a>  
+
+        <a class="nodesk navlinks" href="index.html">Home</a>
+        <a class="nodesk navlinks" href="menu.html">Menu</a>
+        <a class="nodesk navlinks" href="events.html">Upcoming Events</a>
+<!--        <a class="nodesk navlinks" href="lodging.html">Event Lodging</a> -->
+<!--        <a class="nodesk navlinks" href="caterfaq.html">Catering FAQs</a> -->
+        <a class="nodesk navlinks" href="caterinquire.html">Catering Inquiries</a>
+        <a class="nodesk navlinks" href="contact.html">Contact Us</a>
+          </nav>
+      </div>
+  </header>
+
+<h2 id="thks">Thank you for contacting us. We will be in touch with you very soon-  Moe's BBQ</h2>
+
+	<footer>
+		<div id="socmed">
+			<a target="_blank" href="https://www.facebook.com/moesoriginalbbqtahoe/?fref=ts"><img class="socpad" src="images/facebook.png"></a>
+			<a target="_blank" href="https://www.instagram.com/moesoriginalbbqtahoecity/?hl=en"><img class="socpad" src="images/intagram.png"></a>
+		</div>
+		<div id="hours">
+			<h4>Hours:</h4>
+			<h4>Monday-Friday 12pm-8pm</h4>
+			<h4>Saturday-Sunday 11am-9pm</h4>
+			<h4>Bar open till midnight Thursday-Saturday</h4>
+		</div>
+		<div id="footer_copyright" class="clearfix">
+			<p class="clearfix fwords">&copy; Moe's BBQ 2017</p>
+			<a target="_blank" id="acwords" class="clearfix fwords" href="http://alpencreative.com/"><p class="fwords"> Website Created by<img id="aclogo" src="images/AlpenCreativeGeoLogo_Color_v2 copy 2.svg"> AlpenCreative</a></p>
+		</div>
+	</footer>
+
+</body>
+</html>
